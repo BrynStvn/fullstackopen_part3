c8f34b29a012abeee61d39a037cb563e597391b9
BrynStvn <bry.stvn98@gmail.com>
2025-04-20 18:43:10 -0500

    uibuild

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
     return request.method === 'POST' ? JSON.stringify(request.body) : ''
