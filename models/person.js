884c0775a209187ff928e73890b0ffffa3312717
BrynStvn <bry.stvn98@gmail.com>
2025-04-23 21:24:57 -0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -12,13 +12,25 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+  const phoneValidator = (number) => {
+    const regex = /^\d{2,3}-\d+$/
+    return regex.test(number) && number.length >= 8
+  }
+
   const personSchema = new mongoose.Schema({
     name: {
       type: String,
-      minLength: 3,
-      required: true
+      minLength: [3, 'Name must be at least 3 characters long'],
+      required: [true, 'Name is required']
     },
-    number: String,
+    number: {
+      type: String,
+      required: [true, 'Phone number is required'],
+      validate: {
+        validator: phoneValidator,
+        message: props => `${props.value} is not a valid phone number. Use format XX-XXXXXXX or XXX-XXXXXXXX`
+      }
+    }
   })
   
   personSchema.set('toJSON', {
